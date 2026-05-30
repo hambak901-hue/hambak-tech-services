285d8e86f894d87ba53f0e2c9609f1a39b61bb60
hambak901-hue <hambak901@gmail.com>
2026-05-30 10:54:46 +0100

    Update Transaction.js

--- backend/models/Transaction.js
+++ backend/models/Transaction.js
@@ -63,5 +63,5 @@ const transactionSchema = new mongoose.Schema({
   timestamps: true
 });
 
-const Transaction = mongoose.model("Transaction", transactionSchema);
+const Transaction = mongoose.models.Transaction || mongoose.model("Transaction", transactionSchema);
 export default Transaction;
