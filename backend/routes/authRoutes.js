6d01730e6c0a01e3a560eb7bc7e6a793851bc2f8
hambak901-hue <hambak901@gmail.com>
2026-05-30 04:20:56 +0100

    Update authRoutes.js

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,12 +1,12 @@
-import express from "express";
-import { registerUser, loginUser, getMe, logoutUser } from "../controllers/authController.js";
-import { protect } from "../middleware/authMiddleware.js";
+import express from 'express';
+import { registerUser, loginUser, getMyProfile, logoutUser } from '../controllers/authController.js';
+import { protect } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
-router.post("/register", registerUser);
-router.post("/login", loginUser);
-router.get("/me", protect, getMe);
-router.get("/logout", logoutUser);
+router.post('/register', registerUser);
+router.post('/login', loginUser);
+router.get('/me', protect, getMyProfile);
+router.get('/logout', logoutUser);
 
 export default router;
